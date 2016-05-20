77050642de32fe0de9b8fb147f802553b9d06910
Evan Schultz <evan@rangle.io>
2016-05-20 08:27:38 +0200

    2. Status Field
    * Add status field

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -8,13 +8,18 @@ declare const codePush: CodePushCordovaPlugin;
 export class Page1 {
 
   isUpdateAvailable: boolean = false;
-
+  status: string = '';
+  isProcessing: boolean = false;
   constructor(private appRef: ApplicationRef) { }
 
   checkForUpdate() {
+    this.isProcessing = true;
+    this.status = 'Checking ... '
     codePush.checkForUpdate((result) => {
-      this.isUpdateAvailable = result !== null;
-      this.appRef.tick();
+    this.isUpdateAvailable = result !== null;
+    this.status = result === null ? 'Up to Date' : 'Update Available'
+    this.isProcessing = false;
+    this.appRef.tick();
     });
   }
 
