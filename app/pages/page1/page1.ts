88564d962457d8023d75a44729d699105be0f935
Evan Schultz <evan@rangle.io>
2016-05-20 08:32:20 +0200

    4-Add Deployment Selection

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -10,23 +10,27 @@ export class Page1 {
   isUpdateAvailable: boolean = false;
   status: string = '';
   isProcessing: boolean = false;
+  deployment: string = '';
   constructor(private appRef: ApplicationRef) { }
 
-  checkForUpdate() {
+  checkForUpdate(key) {
     this.isProcessing = true;
     this.status = 'Checking ... '
     codePush.checkForUpdate((result) => {
-    this.isUpdateAvailable = result !== null;
-    this.status = result === null ? 'Up to Date' : 'Update Available'
-    this.isProcessing = false;
-    this.appRef.tick();
-    });
+      this.isUpdateAvailable = result !== null;
+      this.status = result === null ? 'Up to Date' : 'Update Available'
+      this.isProcessing = false;
+      this.appRef.tick();
+    },
+      null,
+    key);
   }
 
-  sync() {
+  sync(key) {
     codePush.sync(null, {
       updateDialog: true,
-      installMode: InstallMode.IMMEDIATE
+      installMode: InstallMode.IMMEDIATE,
+      deploymentKey: key
     });
   }
 
