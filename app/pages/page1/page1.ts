c1c9b54b0a574811e298219a9fe56e7352fa965c
Evan Schultz <evan@rangle.io>
2016-05-20 08:44:12 +0200

    Sync Status

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -41,8 +41,40 @@ export class Page1 {
     key);
   }
 
+  syncHandler(status: SyncStatus) {
+    switch (status) {
+      case SyncStatus.UP_TO_DATE:
+        this.status = 'Up-to-date';
+        break;
+      case SyncStatus.UPDATE_INSTALLED:
+        this.status = 'Update Installed';
+        break;
+      case SyncStatus.UPDATE_IGNORED:
+        this.status = 'Update Ignored';
+        break;
+      case SyncStatus.ERROR:
+        this.status = 'Error';
+        break;
+      case SyncStatus.IN_PROGRESS:
+        this.status = 'In Progress';
+        break;
+      case SyncStatus.CHECKING_FOR_UPDATE:
+        this.status = 'Checking for Update';
+        break;
+      case SyncStatus.AWAITING_USER_ACTION:
+        this.status = 'Awaiting User Action';
+        break;
+      case SyncStatus.DOWNLOADING_PACKAGE:
+        this.status = 'Downloading Package';
+        break;
+      case SyncStatus.INSTALLING_UPDATE:
+        this.status = 'Installing Update';
+        break;
+    }
+    this.appRef.tick();
+  }
   sync(key) {
-    codePush.sync(null, {
+     codePush.sync((status) => this.syncHandler(status), {
       updateDialog: true,
       installMode: InstallMode.IMMEDIATE,
       deploymentKey: key
