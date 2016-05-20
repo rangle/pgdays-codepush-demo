31191797a89e078db3c53c469244723fa8bf4baa
Evan Schultz <evan@rangle.io>
2016-05-20 08:19:11 +0200

    Settings Tab

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,11 +1,28 @@
 import {Page} from 'ionic-angular';
-
+import {ApplicationRef} from '@angular/core';
+declare const codePush: CodePushCordovaPlugin;
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html',
 })
 export class Page1 {
-  constructor() {
 
+  isUpdateAvailable: boolean = false;
+
+  constructor(private appRef: ApplicationRef) { }
+
+  checkForUpdate() {
+    codePush.checkForUpdate((result) => {
+      this.isUpdateAvailable = result !== null;
+      this.appRef.tick();
+    });
   }
+
+  sync() {
+    codePush.sync(null, {
+      updateDialog: true,
+      installMode: InstallMode.IMMEDIATE
+    });
+  }
+
 }
